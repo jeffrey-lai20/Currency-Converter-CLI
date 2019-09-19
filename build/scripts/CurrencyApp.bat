205db3a9af6a0be9a8ea9c7792db580ddf73e828
OwenTsoi <ttso7897@uni.sydney.edu.au>
2019-09-19 11:24:58 +1000

    merging

--- build/scripts/CurrencyApp.bat
+++ build/scripts/CurrencyApp.bat
@@ -1,19 +1,3 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
-
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
@@ -79,7 +63,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\CurrencyApp-1.0-SNAPSHOT.jar;%APP_HOME%\lib\javafx-media-12.0.2-linux.jar;%APP_HOME%\lib\javafx-controls-12.0.2-linux.jar;%APP_HOME%\lib\javafx-graphics-12.0.2-linux.jar;%APP_HOME%\lib\javafx-graphics-12.0.2.jar;%APP_HOME%\lib\javafx-base-12.0.2-linux.jar;%APP_HOME%\lib\javafx-base-12.0.2.jar
+set CLASSPATH=%APP_HOME%\lib\CurrencyApp-1.0-SNAPSHOT.jar;%APP_HOME%\lib\javafx-media-12.0.2-mac.jar;%APP_HOME%\lib\javafx-controls-12.0.2-mac.jar;%APP_HOME%\lib\javafx-graphics-12.0.2-mac.jar;%APP_HOME%\lib\javafx-graphics-12.0.2.jar;%APP_HOME%\lib\javafx-base-12.0.2-mac.jar;%APP_HOME%\lib\javafx-base-12.0.2.jar
 
 @rem Execute CurrencyApp
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %CURRENCY_APP_OPTS%  -classpath "%CLASSPATH%" App %CMD_LINE_ARGS%
